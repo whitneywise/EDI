214dea26a6d9de27fe9a4b5dbf9ad13c595d941f
whitneywise <wisewhitney8@gmail.com>
2021-12-03 12:58:50 -0500

    gif

---
+++ CreativeCoding/p5/website backround/sketch.js
@@ -0,0 +1,32 @@
+let gif;
+
+function preload (){
+  gif = loadImage ('grid.gif');
+}
+
+function setup() { //runs once
+  createCanvas(windowWidth, windowHeight);
+  fill('blue');
+  strokeWeight(0);
+}
+
+function draw() {
+
+background (250);
+
+var num = 2.44; //variable for the number of squares in the array
+var sideLen = windowWidth/num;//variable for the side lenth of each square
+
+  for (var y = 0; y < windowHeight; y = y + sideLen) {//loop to create rows in the y direction
+     for (var x = 0; x < windowWidth; x = x + sideLen) {// loop to create a row of squares in the x direction0
+image (gif, x, y);
+  }
+}
+
+//noLoop ();
+
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
