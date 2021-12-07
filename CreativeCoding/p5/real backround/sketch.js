c18e899105c158d7d316b3370819f0b6c7ab72b6
whitneywise <wisewhitney8@gmail.com>
2021-12-07 08:45:30 -0500

    wb

--- CreativeCoding/p5/real backround/sketch.js
+++ CreativeCoding/p5/real backround/sketch.js
@@ -14,7 +14,7 @@ function draw() {
 
 background (250);
 
-var num = 2.44; //variable for the number of squares in the array
+var num =4.8; //variable for the number of squares in the array
 var sideLen = windowWidth/num;//variable for the side lenth of each square
 
   for (var y = 0; y < windowHeight; y = y + sideLen) {//loop to create rows in the y direction
